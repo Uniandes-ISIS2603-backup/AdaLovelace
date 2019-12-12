deb0f7416604779acee332a435c71bc01c64de08
Daniel Galindo Ruiz <d.galindo@uniandes.edu.co>
2019-12-12 09:57:34 -0500

    saleEntity creada

--- backstepbystep-back/src/main/java/co/edu/uniandes/csw/bookstore/entities/SaleEntity.java
+++ backstepbystep-back/src/main/java/co/edu/uniandes/csw/bookstore/entities/SaleEntity.java
@@ -5,10 +5,72 @@
  */
 package co.edu.uniandes.csw.bookstore.entities;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 /**
  *
  * @author Ada Lovelace
  */
-public class SaleEntity {
+@Entity
+public class SaleEntity extends BaseEntity {
     
+    
+    private int value;
+    
+    @Temporal(TemporalType.DATE)
+    private Date date;
+    
+    @OneToMany
+    private List<BookEntity> books = new ArrayList<>();
+    
+    //@OneToMany
+    //private List<ClientEntity> clients = new ArrayList<>();
+
+    /**
+     * @return the value
+     */
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    /**
+     * @return the date
+     */
+    public Date getDate() {
+        return date;
+    }
+
+    /**
+     * @param date the date to set
+     */
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    /**
+     * @return the books
+     */
+    public List<BookEntity> getBooks() {
+        return books;
+    }
+
+    /**
+     * @param books the books to set
+     */
+    public void setBooks(List<BookEntity> books) {
+        this.books = books;
+    }
 }
