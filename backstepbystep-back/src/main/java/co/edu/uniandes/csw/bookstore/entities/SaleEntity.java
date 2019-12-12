754ed349ef8b853348af811b83e28ff151e67e72
Daniel Galindo Ruiz <d.galindo@uniandes.edu.co>
2019-12-12 17:52:04 -0500

    siu

--- backstepbystep-back/src/main/java/co/edu/uniandes/csw/bookstore/entities/SaleEntity.java
+++ backstepbystep-back/src/main/java/co/edu/uniandes/csw/bookstore/entities/SaleEntity.java
@@ -8,6 +8,7 @@ package co.edu.uniandes.csw.bookstore.entities;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
@@ -30,7 +31,7 @@ public class SaleEntity extends BaseEntity implements Serializable{
     private List<BookEntity> books = new ArrayList<>();
     
     @PodamExclude
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.PERSIST)
     private ClientEntity client;
 
     /**
